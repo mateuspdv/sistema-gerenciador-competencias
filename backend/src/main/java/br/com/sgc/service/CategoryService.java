7683a614cb92572793219e2dcf68adf861d8cb33
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2022-10-29 14:02:46 -0300

    Tabela de domínio fixo Categoria implementada

---
+++ backend/src/main/java/br/com/sgc/service/CategoryService.java
@@ -0,0 +1,13 @@
+package br.com.sgc.service;
+
+import br.com.sgc.service.dto.CategoryDto;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDto> findAll();
+
+    CategoryDto findById(Long idCategory);
+
+}
---
+++ backend/src/main/java/br/com/sgc/util/MessageUtil.java
@@ -0,0 +1,12 @@
+package br.com.sgc.util;
+
+public class MessageUtil {
+
+    /* Messages for entities not found */
+    public static final String CATEGORY_NOT_FOUND = "Categoria não encontrada!";
+
+    /* Messages for ApiOperation annotation */
+    public static final String OPERATION_CATEGORY_FIND_ALL = "Retorna todas as categorias cadastradas no sistema";
+    public static final String OPERATION_CATEGORY_FIND_BY_ID = "Retorna uma categoria cadastrada no sistema pelo id";
+
+}
