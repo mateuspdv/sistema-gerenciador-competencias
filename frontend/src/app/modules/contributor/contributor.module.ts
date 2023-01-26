34b7f60a4be190d46877d0050a9607176f8330dd
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2023-01-26 17:50:25 -0300

    Adicionado SharedModule

--- frontend/src/app/modules/contributor/contributor.module.ts
+++ frontend/src/app/modules/contributor/contributor.module.ts
@@ -1,23 +1,21 @@
+import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ContributorRoutingModule } from './contributor-routing.module';
 import { ContributorListComponent } from './components/contributor-list/contributor-list.component';
 import { ContributorFormComponent } from './components/contributor-form/contributor-form.component';
 import { ContributorViewComponent } from './components/contributor-view/contributor-view.component';
-import { TableModule } from 'primeng/table';
 
 
 @NgModule({
-  declarations: [
-    ContributorListComponent,
-    ContributorFormComponent,
-    ContributorViewComponent
-  ],
-  imports: [
-    CommonModule,
-    ContributorRoutingModule,
-    TableModule
-  ]
+    declarations: [
+        ContributorListComponent,
+        ContributorFormComponent,
+        ContributorViewComponent
+    ],
+    imports: [
+        ContributorRoutingModule,
+        SharedModule
+    ]
 })
 export class ContributorModule { }
