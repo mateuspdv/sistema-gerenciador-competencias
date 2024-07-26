05821c4cc7a7dcc067ed42695e98d4f3ab68202b
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2024-07-25 23:45:36 -0300

    chore: initialize backend setup

---
+++ src/test/java/br/com/sgc/SistemaGerenciadorCompetenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorCompetenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
