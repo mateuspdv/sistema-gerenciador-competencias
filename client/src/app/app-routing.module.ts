a12caa1c9cf9dcf91e32889dbb82be9d19fe0f3f
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2024-08-20 23:24:24 -0300

    chore: initialize frontend setup

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppLayoutComponent } from "./layout/app.layout.component";
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppLayoutComponent,
+                children: []
+            },
+            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
+            { path: '**', redirectTo: '/notfound' },
+        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppLayoutModule } from './layout/app.layout.module';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        AppLayoutModule
+    ],
+    providers: [
+        { provide: LocationStrategy, useClass: PathLocationStrategy },
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
